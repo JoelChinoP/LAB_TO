14753c791aa0b02ab5588a2164950b0cbc77fe72
Joel <jchinop@unsa.edu.pe>
2024-10-24 01:00:22 -0500

    ejercicio 2

--- Lab - 04/Ex02/main.cpp
+++ Lab - 04/Ex02/main.cpp
@@ -7,6 +7,15 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
+
+/*
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
 
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
--- Lab - 04/Ex02/mainwindow.cpp
+++ Lab - 04/Ex02/mainwindow.cpp
@@ -1,24 +1,19 @@
+// mainwindow.cpp
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include "./ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-    // Agregar países a la lista
-    ui->listWidget->addItem("Peru");
-    ui->listWidget->addItem("France");
-    ui->listWidget->addItem("Japan");
-
-    // Asignar información de idioma y capital a cada país
-    countryInfo["Peru"] = qMakePair(QString("Spanish"), QString("Lima"));
-    countryInfo["France"] = qMakePair(QString("French"), QString("Paris"));
-    countryInfo["Japan"] = qMakePair(QString("Japanese"), QString("Tokyo"));
-
-    // Conectar la señal de selección de país con el slot
-    connect(ui->listWidget, &QListWidget::itemClicked, this, &MainWindow::onCountrySelected);
+    initializeCountryData();
+    setupUI();
+    
+    // Conectar la señal de selección con el slot
+    connect(countryList, &QListWidget::itemClicked, 
+            this, &MainWindow::onCountrySelected);
 }
 
 MainWindow::~MainWindow()
@@ -26,12 +21,64 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::onCountrySelected(QListWidgetItem *item)
+void MainWindow::initializeCountryData()
+{
+    // Inicializar datos de países: País -> {Idioma, Capital}
+    countryData["Perú"] = {"Español", "Lima"};
+    countryData["Brasil"] = {"Portugués", "Brasilia"};
+    countryData["Francia"] = {"Francés", "París"};
+    countryData["Japón"] = {"Japonés", "Tokio"};
+    countryData["Italia"] = {"Italiano", "Roma"};
+    countryData["Alemania"] = {"Alemán", "Berlín"};
+    countryData["China"] = {"Mandarín", "Pekín"};
+    countryData["España"] = {"Español", "Madrid"};
+}
+
+void MainWindow::setupUI()
+{
+    // Crear widget central y layout
+    QWidget *centralWidget = new QWidget(this);
+    QHBoxLayout *mainLayout = new QHBoxLayout(centralWidget);
+    
+    // Configurar lista de países
+    countryList = new QListWidget(this);
+    countryList->setMinimumWidth(200);
+    for (const auto& country : countryData) {
+        countryList->addItem(country.first);
+    }
+    
+    // Configurar panel de información
+    QWidget *infoPanel = new QWidget(this);
+    QVBoxLayout *infoPanelLayout = new QVBoxLayout(infoPanel);
+    
+    QLabel *titleLabel = new QLabel("Información del País", this);
+    titleLabel->setStyleSheet("font-size: 16px; font-weight: bold;");
+    
+    languageLabel = new QLabel("Idioma: ", this);
+    capitalLabel = new QLabel("Capital: ", this);
+    
+    languageLabel->setStyleSheet("font-size: 14px; margin: 10px;");
+    capitalLabel->setStyleSheet("font-size: 14px; margin: 10px;");
+    
+    infoPanelLayout->addWidget(titleLabel);
+    infoPanelLayout->addWidget(languageLabel);
+    infoPanelLayout->addWidget(capitalLabel);
+    infoPanelLayout->addStretch();
+    
+    // Agregar widgets al layout principal
+    mainLayout->addWidget(countryList);
+    mainLayout->addWidget(infoPanel);
+    
+    setCentralWidget(centralWidget);
+    setWindowTitle("Información de Países");
+    resize(600, 400);
+}
+
+void MainWindow::onCountrySelected(QListWidgetItem* item)
 {
     QString country = item->text();
-    if (countryInfo.contains(country)) {
-        // Mostrar el idioma y la capital correspondiente
-        ui->labelLanguage->setText("Language: " + countryInfo[country].first);
-        ui->labelCapital->setText("Capital: " + countryInfo[country].second);
+    if (countryData.find(country) != countryData.end()) {
+        languageLabel->setText("Idioma: " + countryData[country].first);
+        capitalLabel->setText("Capital: " + countryData[country].second);
     }
 }
